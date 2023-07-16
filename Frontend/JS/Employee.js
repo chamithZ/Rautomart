4f393325a79e7e40bbb82d2a8e3fcefa022b739b
it21156960 <chamith227@gmail.com>
2023-07-16 22:30:06 +0530

    saveDone

--- Frontend/JS/Employee.js
+++ Frontend/JS/Employee.js
@@ -1,15 +1,32 @@
-function saveEmp(){
+function saveEmp() {
+  let name = $('#name').val();
+  let contact = $('#contact').val();
+  let address = $('#address').val();
 
-    let name= $('#name').val();
-    let contact= $('#contact').val();
-    let address= $('#address').val();
+  // Create an object to hold the data
+  let data = {
+    empId: "",
+    name: name,
+    contactNo: contact,
+    acNo: address
+  };
 
-    console.log(name)
-
-
-
-}
-
-function updateEmp(){
-
+  // Send the data using Ajax
+  $.ajax({
+    url: 'http://localhost:8080/api/v1/employee/saveEmployee',
+    method: 'POST',
+    contentType: 'application/json',
+    crossDomain: true,
+    xhrFields: {
+      withCredentials: true
+    },
+    data: JSON.stringify(data),
+    success: function(response) {
+      alert('saved');
+      getAllEmployees();
+    },
+    error: function(xhr, status, error) {
+      alert('Error');
+    }
+  });
 }
